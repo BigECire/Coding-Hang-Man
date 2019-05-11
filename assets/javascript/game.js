b95d373ae0884fd328acd29783e6b4b9aee30637
BigECire <ericsanders5800@gmail.com>
2019-05-11 18:59:58 -0500

    added score, repeat guess list, and start with any key

--- assets/javascript/game.js
+++ assets/javascript/game.js
@@ -2,8 +2,12 @@
 var wordBank = ["abcdefghi jklmnopqrs tuvwxyz", "javascript"]
 var theWord = ""
 var emptyWord = ""
+var guessedLetters = ""
+var start = true
 var used = false
 var guess = 6
+var win = 0
+var lose = 0
 
 function startUp() {
     theWord = wordBank[Math.floor(Math.random() * wordBank.length)]
@@ -19,19 +23,24 @@ function startUp() {
     console.log(emptyWord)
 }
 
-startUp()
-
 function reset() {
      theWord = ""
      emptyWord = ""
+     guessedLetters = ""
      guess = 6
     
     startUp()
 }
 
+
 document.onkeyup = function (event) {
     var playerkey = event.key.toLowerCase();
     var word = ""
+    if (start) {
+    start = false
+startUp()
+}
+else {
     if ((playerkey === "a") || (playerkey === "b") || (playerkey === "c") || (playerkey === "d") || (playerkey === "e") || (playerkey === "f") || (playerkey === "g") || (playerkey === "h") || (playerkey === "i") || (playerkey === "j") || (playerkey === "k") || (playerkey === "l") || (playerkey === "m") || (playerkey === "n") || (playerkey === "o") || (playerkey === "p") || (playerkey === "q") || (playerkey === "r") || (playerkey === "s") || (playerkey === "t") || (playerkey === "u") || (playerkey === "v") || (playerkey === "w") || (playerkey === "x") || (playerkey === "y") || (playerkey === "z")) {
         for (var i = 0; i < theWord.length; i++) {
             if (theWord.charAt(i) === playerkey) {
@@ -42,21 +51,33 @@ document.onkeyup = function (event) {
                 word += emptyWord.charAt(i)
             }
         };
-        if (!used) {
+        var repeat = false
+        for(var i = 0; i < guessedLetters.length; i++){
+            if (guessedLetters.charAt(i) === playerkey.toUpperCase()){
+                repeat = true
+            }
+        }
+        
+        if ((!used) && (!repeat)) {
             guess = guess - 1
+            guessedLetters += playerkey.toUpperCase() + " "
+            console.log(guessedLetters)
         }
         used = false
         emptyWord = word
-
+console.log(emptyWord + " " + guess)
         if(emptyWord === theWord){
             alert("You Win!")
+            win++
             reset()
         }
         if(guess === 0){
             alert("You Lose! The answer was " + theWord)
+            lose++
             reset()
         }
 
-        console.log(emptyWord + " " + guess)
+        
     };
+}
 };
